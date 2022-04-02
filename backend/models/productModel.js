211326a25c174b1a80ee4f52c9ec593fb9f57052
amitmungare <amungare27@gmail.com>
2022-04-02 21:19:57 +0530

    product APIs created

---
+++ backend/models/productModel.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    images:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:true,
+    },
+    Stock:{
+        type:Number,
+        required:true,
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+
+module.exports =mongoose.model("Product", productSchema)
